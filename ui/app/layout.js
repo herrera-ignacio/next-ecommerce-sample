a189d43d5aae547e87b7c1fce1bc0e5dc525a819
Ignacio Herrera <ignacioromanherrera@gmail.com>
2024-05-28 23:15:13 -0300

    NextAuthProvider separated into its own component

--- ui/app/layout.js
+++ ui/app/layout.js
@@ -1,20 +1,19 @@
-"use client";
 import "./globals.css";
 import "bootstrap-material-design/dist/css/bootstrap-material-design.min.css";
 import TopNav from "@/components/TopNav";
 import { Toaster } from "react-hot-toast";
-import { SessionProvider } from "next-auth/react";
+import { NextAuthProvider } from "@/components/NextAuthProvider";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <SessionProvider>
-        <body>
+      <body>
+        <NextAuthProvider>
           <TopNav />
           <Toaster />
           {children}
-        </body>
-      </SessionProvider>
+        </NextAuthProvider>
+      </body>
     </html>
   );
 }
