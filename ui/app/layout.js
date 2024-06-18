aa0c3950a20692627a96331023984910728f60f6
Ignacio Herrera <ignacioromanherrera@gmail.com>
2024-06-17 22:46:47 -0300

    Admin dashboard layout

--- ui/app/layout.js
+++ ui/app/layout.js
@@ -1,6 +1,6 @@
 import "./globals.css";
 import "bootstrap-material-design/dist/css/bootstrap-material-design.min.css";
-import TopNav from "@/components/TopNav";
+import TopNav from "@/components/nav/TopNav";
 import { Toaster } from "react-hot-toast";
 import { NextAuthProvider } from "@/components/NextAuthProvider";
 
