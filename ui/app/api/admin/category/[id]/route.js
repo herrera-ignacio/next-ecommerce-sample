64058607764c1e99cfd2270b59e0e863d5cd2b0c
Ignacio Herrera <ignacioromanherrera@gmail.com>
2024-06-23 22:18:09 -0300

    Category CRUD API

---
+++ ui/app/api/admin/category/[id]/route.js
@@ -0,0 +1,36 @@
+import dbConnect from "@/utils/dbConnect";
+import slugify from "slugify";
+import { NextResponse } from "next/server";
+import Category from "@/models/category";
+
+export async function PUT(req, context) {
+  await dbConnect();
+  const body = await req.json();
+  const { name } = body;
+
+  try {
+    const updatingCategory = await Category.findByIdAndUpdate(
+      context.params.id,
+      {
+        ...body,
+        slug: slugify(name),
+      },
+      { new: true },
+    );
+    return NextResponse.json(updatingCategory);
+  } catch (err) {
+    return NextResponse.json(err.message, { status: 500 });
+  }
+}
+
+export async function DELETE(req, context) {
+  await dbConnect();
+  try {
+    const deletingCategory = await Category.findByIdAndDelete(
+      context.params.id,
+    );
+    return NextResponse.json(deletingCategory);
+  } catch (err) {
+    return NextResponse.json(err.message, { status: 500 });
+  }
+}
